46d32beb0ad7387cfe35e5799134a08718056875
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-17 23:00:00 +0300

    Display the user avatar

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,7 +1,6 @@
 import { useHistory } from 'react-router-dom'
-import { useIsAuthenticated } from 'react-auth-kit'
-import { useSignOut } from 'react-auth-kit'
-import { PageHeader as AntPageHeader, Tag, Tooltip, Button } from 'antd'
+import { useIsAuthenticated, useAuthUser, useSignOut } from 'react-auth-kit'
+import { PageHeader as AntPageHeader, Tag, Tooltip, Avatar, Button } from 'antd'
 import { HomeOutlined, LogoutOutlined } from '@ant-design/icons'
 import packageInfo from '../../../package.json'
 import './PageHeader.css'
@@ -9,6 +8,7 @@ import './PageHeader.css'
 export function PageHeader() {
   const history = useHistory()
   const isAuthenticated = useIsAuthenticated()
+  const auth = useAuthUser()
   const signOut = useSignOut()
   return (
     <AntPageHeader
@@ -18,7 +18,14 @@ export function PageHeader() {
       onBack={() => history.push('/')}
       extra={[
         isAuthenticated() && (
-          <Tooltip key="1" title="Log out">
+          <Tooltip key="1" title={auth().Username}>
+            <Avatar className="page-header-user-avatar">
+              {auth().Username.toUpperCase().charAt(0)}
+            </Avatar>
+          </Tooltip>
+        ),
+        isAuthenticated() && (
+          <Tooltip key="2" title="Log out">
             <Button icon={<LogoutOutlined />} onClick={() => signOut()} />
           </Tooltip>
         ),
