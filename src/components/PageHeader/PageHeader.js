d6177142bc40a680cf3db5ee6fe810b8f01c5737
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-17 23:00:00 +0300

    Implement the signing out

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,17 +1,28 @@
 import { useHistory } from 'react-router-dom'
-import { PageHeader as AntPageHeader, Tag } from 'antd'
-import { HomeOutlined } from '@ant-design/icons'
+import { useIsAuthenticated } from 'react-auth-kit'
+import { useSignOut } from 'react-auth-kit'
+import { PageHeader as AntPageHeader, Tag, Tooltip, Button } from 'antd'
+import { HomeOutlined, LogoutOutlined } from '@ant-design/icons'
 import packageInfo from '../../../package.json'
 import './PageHeader.css'
 
 export function PageHeader() {
   const history = useHistory()
+  const isAuthenticated = useIsAuthenticated()
+  const signOut = useSignOut()
   return (
     <AntPageHeader
       backIcon={<HomeOutlined />}
       title="go-exercises"
       tags={<Tag>{packageInfo.version}</Tag>}
       onBack={() => history.push('/')}
+      extra={[
+        isAuthenticated() && (
+          <Tooltip key="1" title="Log out">
+            <Button icon={<LogoutOutlined />} onClick={() => signOut()} />
+          </Tooltip>
+        ),
+      ]}
     />
   )
 }
