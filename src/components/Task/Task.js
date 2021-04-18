876918452cde758dd6155f743f9a990f4f726f22
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-18 23:00:00 +0300

    Add the loading of the task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,6 +1,38 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useAuthHeader } from 'react-auth-kit'
+import { message } from 'antd'
 
 export function Task() {
+  const [loading, setLoading] = useState(false)
+  const [task, setTask] = useState(null)
   const { id } = useParams()
+  const authHeader = useAuthHeader()
+
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+
+      try {
+        const response = await fetch(`/api/v1/tasks/${id}`, {
+          method: 'GET',
+          headers: { Authorization: authHeader() },
+        })
+        if (!response.ok) {
+          const errMessage = await response.text()
+          throw new Error(errMessage)
+        }
+
+        const task = await response.json()
+        setTask(task)
+        console.log(task)
+      } catch (exception) {
+        message.error(exception.toString())
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return `Task #${id}`
 }
