470b0c02313971d55ce81137ee6d29c9ed85508d
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-19 23:00:00 +0300

    Add the task form for task creation

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,3 +1,64 @@
+import { useState } from 'react'
+import { useAuthHeader } from 'react-auth-kit'
+import { useHistory } from 'react-router-dom'
+import { Spin, Form, Input, Button, message } from 'antd'
+
 export function TaskForm() {
-  return 'Content'
+  const [loading, setLoading] = useState(false)
+  const authHeader = useAuthHeader()
+  const history = useHistory()
+
+  return (
+    <Spin spinning={loading}>
+      <Form
+        layout="vertical"
+        onFinish={async data => {
+          setLoading(true)
+
+          try {
+            const response = await fetch('/api/v1/tasks/', {
+              method: 'POST',
+              headers: {
+                Authorization: authHeader(),
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+            })
+            if (!response.ok) {
+              const errMessage = await response.text()
+              throw new Error(errMessage)
+            }
+
+            const task = await response.json()
+            history.push(`/tasks/${task.ID}`)
+          } catch (exception) {
+            setLoading(false)
+            message.error(exception.toString())
+          }
+        }}
+      >
+        <Form.Item label="Title" name="title">
+          <Input />
+        </Form.Item>
+
+        <Form.Item label="Description" name="description">
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item label="Boilerplate code" name="boilerplateCode">
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item label="Test cases" name="testCases">
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Create task
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
+  )
 }
