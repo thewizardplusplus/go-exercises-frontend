b18443929a77449f89582912b220c65a5f61738f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-21 23:00:00 +0300

    Add the loading of the solutions to the SolutionGroup component

--- src/components/SolutionGroup/SolutionGroup.js
+++ src/components/SolutionGroup/SolutionGroup.js
@@ -1,3 +1,39 @@
+import { useState, useEffect } from 'react'
+import { useAuthHeader } from 'react-auth-kit'
+import { message } from 'antd'
+
 export function SolutionGroup(props) {
+  const [loading, setLoading] = useState(false)
+  const [solutions, setSolutions] = useState([])
+  const authHeader = useAuthHeader()
+
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+
+      try {
+        const response = await fetch(
+          `/api/v1/tasks/${props.taskID}/solutions/`,
+          {
+            method: 'GET',
+            headers: { Authorization: authHeader() },
+          },
+        )
+        if (!response.ok) {
+          const errMessage = await response.text()
+          throw new Error(errMessage)
+        }
+
+        const solutions = await response.json()
+        setSolutions(solutions)
+        console.log(solutions)
+      } catch (exception) {
+        message.error(exception.toString())
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return 'Content'
 }
