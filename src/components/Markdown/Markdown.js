d9ab26a6d23fff83513cdc37fe3094a75b4c5507
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-09 23:00:00 +0300

    Support syntax highlighting in the Markdown component

--- src/components/Markdown/Markdown.js
+++ src/components/Markdown/Markdown.js
@@ -1,11 +1,35 @@
 import { Typography } from 'antd'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export function Markdown(props) {
   return (
     <Typography.Paragraph>
-      <ReactMarkdown remarkPlugins={[gfm]}>{props.content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[gfm]}
+        components={{
+          pre: 'div',
+
+          code({ inline, className, children }) {
+            const languageMatch = (className || '').match(/language-(\w+)/)
+            return !inline ? (
+              <SyntaxHighlighter
+                language={languageMatch ? languageMatch[1] : 'text'}
+                style={tomorrow}
+                showLineNumbers
+              >
+                {children.toString().replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code>{children}</code>
+            )
+          },
+        }}
+      >
+        {props.content}
+      </ReactMarkdown>
     </Typography.Paragraph>
   )
 }
