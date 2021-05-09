0396b5a45fe6b458de1efcf2fbde71adb9d67fe0
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-09 23:00:00 +0300

    Add the status sign to the TaskGroup component

--- src/components/TaskGroup/TaskGroup.js
+++ src/components/TaskGroup/TaskGroup.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useAuthHeader } from 'react-auth-kit'
 import { useHistory } from 'react-router-dom'
-import { List, Button, message } from 'antd'
+import { List, Avatar, Typography, Button, message } from 'antd'
+import {
+  CheckOutlined,
+  CloseOutlined,
+  QuestionOutlined,
+} from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import { TaskDetails } from '../TaskDetails/TaskDetails.js'
 import './TaskGroup.css'
@@ -47,6 +52,27 @@ export function TaskGroup() {
         renderItem={task => (
           <List.Item>
             <List.Item.Meta
+              avatar={
+                <Avatar
+                  className="task-group-task-avatar"
+                  icon={
+                    task.Status === 2 ? (
+                      // solved
+                      <Typography.Text type="success">
+                        <CheckOutlined />
+                      </Typography.Text>
+                    ) : task.Status === 1 ? (
+                      // not solved
+                      <Typography.Text type="danger">
+                        <CloseOutlined />
+                      </Typography.Text>
+                    ) : (
+                      // no solutions
+                      <QuestionOutlined />
+                    )
+                  }
+                />
+              }
               title={
                 <Link
                   to={`/tasks/${task.ID}`}
