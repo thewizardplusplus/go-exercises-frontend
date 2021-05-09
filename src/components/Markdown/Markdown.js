5fda3edeae30cdb555ae400d89d344d02a5c5b55
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-09 23:00:00 +0300

    Fix styles of tables in the Markdown component

--- src/components/Markdown/Markdown.js
+++ src/components/Markdown/Markdown.js
@@ -1,12 +1,14 @@
-import { Typography } from 'antd'
+import { Typography, Table } from 'antd'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import React from 'react'
+import './Markdown.css'
 
 export function Markdown(props) {
   return (
-    <Typography.Paragraph>
+    <Typography.Paragraph className="markdown-paragraph">
       <ReactMarkdown
         remarkPlugins={[gfm]}
         components={{
@@ -24,6 +26,53 @@ export function Markdown(props) {
               </SyntaxHighlighter>
             ) : (
               <code>{children}</code>
+            )
+          },
+
+          table({ children }) {
+            let columns = []
+            let dataSource = []
+            React.Children.forEach(children, ({ props: { children } }) => {
+              React.Children.forEach(children, ({ props: { children } }) => {
+                let columnData = {}
+                React.Children.forEach(
+                  children,
+                  ({ type, props: { children } }) => {
+                    switch (type) {
+                      case 'th':
+                        columns.push({
+                          dataIndex: `column${columns.length}`,
+                          title: children.toString(),
+                        })
+                        break
+                      case 'td':
+                        columnData = {
+                          ...columnData,
+                          [`column${
+                            Object.keys(columnData).length
+                          }`]: children.toString(),
+                        }
+                        break
+                      default:
+                        break
+                    }
+                  },
+                )
+                if (Object.keys(columnData).length !== 0) {
+                  dataSource.push({
+                    ...columnData,
+                    key: dataSource.length.toString(),
+                  })
+                }
+              })
+            })
+
+            return (
+              <Table
+                columns={columns}
+                dataSource={dataSource}
+                pagination={false}
+              />
             )
           },
         }}
