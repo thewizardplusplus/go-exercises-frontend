15b32bda67a430b15d95e251bdb0b4a85cc9d593
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-10 23:00:00 +0300

    Add the Editor component

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,22 @@
+import AceEditor from 'react-ace'
+import 'ace-builds/webpack-resolver'
+import 'ace-builds/src-noconflict/mode-markdown'
+import 'ace-builds/src-noconflict/mode-golang'
+import 'ace-builds/src-noconflict/mode-json'
+import 'ace-builds/src-noconflict/theme-monokai'
+
+export function Editor(props) {
+  return (
+    <AceEditor
+      theme="monokai"
+      width={'100%'}
+      minLines={5}
+      maxLines={Number.POSITIVE_INFINITY}
+      fontSize={16}
+      tabSize={2}
+      wrapEnabled={true}
+      setOptions={{ showInvisibles: true, indentedSoftWrap: false }}
+      {...props}
+    />
+  )
+}
